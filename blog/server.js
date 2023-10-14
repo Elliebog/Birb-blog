5aff0d73ad4fc09b8de65a5ef75ef4100ce26f95
Ellie Bogner <ellie.bogner@gmail.com>
2023-10-15 00:24:07 +0200

    templating basic functionality

--- blog/server.js
+++ blog/server.js
@@ -1,10 +1,34 @@
 import Fastify from 'fastify'
 import fs from 'fs'
+import doT from 'dot'
+import fastify_static from '@fastify/static'
+import path from 'node:path'
 
 const fastify = Fastify({ logger: true })
 
+//setup Static files
+fastify.register(fastify_static, {
+    root: path.resolve('./web/static'),
+    prefix: '/static'
+})
+
 fastify.get('/', async function handler(request, reply) {
-    sendFile(reply, 'text/html', 'web/test.html')
+    const template = fs.readFileSync('web/templates/main.html')
+    const contentdata = fs.readFileSync('web/test.html')
+    //apply the Template
+    var templateFn = doT.template(template.toString())
+
+    request.log.info(contentdata.toString())
+
+    //send the compiled file back
+    reply.type('text/html').send(templateFn({ content: contentdata.toString() }))
+    return reply
+})
+
+fastify.get('/static/:filetype/:filename', async function handler(request, reply) {
+    const { filetype , filename } = request.params
+    
+    reply.sendFile(filetype + '/' + filename)
     return reply
 })
 
@@ -13,15 +37,4 @@ try {
 } catch (err) {
     fastify.log.error(err)
     process.exit(1)
-}
-
-/**
- * Sends a file using the reply object
- * @param {FastifyReply} reply 
- * @param {String} applicationType 
- * @param {String} filePath 
- */
-async function sendFile(reply, applicationType, filePath) {
-    const stream = fs.createReadStream(filePath)
-    reply.type(applicationType).send(stream)
 }
